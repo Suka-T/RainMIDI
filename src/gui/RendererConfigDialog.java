898a9728d9f1994cfc1a12eec1505f638ee6f7ba
Suka-T <absolute6626x@gmail.com>
2026-04-04 12:59:31 +0900

    設定画面からD&Dをできるようにした

--- src/gui/RendererConfigDialog.java
+++ src/gui/RendererConfigDialog.java
@@ -7,6 +7,8 @@ import java.awt.Desktop;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -49,6 +51,7 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingConstants;
+import javax.swing.TransferHandler;
 import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.EtchedBorder;
@@ -286,7 +289,8 @@ public class RendererConfigDialog extends JFrame implements ActionListener {
         this.setIconImages(icons);
         
         initialized.set(false);
-
+        
+        this.setTransferHandler(new DropFileHandler());
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -1429,5 +1433,69 @@ public class RendererConfigDialog extends JFrame implements ActionListener {
 
     public boolean isCommitClose() {
         return isCommitClose;
+    }
+    
+
+    /**
+     *
+     * ドラッグ＆ドロップハンドラー
+     *
+     */
+    public class DropFileHandler extends TransferHandler {
+        /**
+         * ドロップされたものを受け取るか判断 (アイテムのときだけ受け取る)
+         */
+        @Override
+        public boolean canImport(TransferSupport support) {
+            if (support.isDrop() == false) {
+                // ドロップ操作でない場合は受け取らない
+                return false;
+            }
+
+            if (support.isDataFlavorSupported(DataFlavor.javaFileListFlavor) == false) {
+                // ファイルでない場合は受け取らない
+                return false;
+            }
+
+            return true;
+        }
+
+        /**
+         * ドロップされたアイテムを受け取る
+         */
+        @Override
+        public boolean importData(TransferSupport support) {
+            // ドロップアイテム受理の確認
+            if (canImport(support) == false) {
+                return false;
+            }
+
+            // ドロップ処理
+            Transferable t = support.getTransferable();
+            try {
+                // ドロップアイテム取得
+                catchLoadItem(t.getTransferData(DataFlavor.javaFileListFlavor));
+                return true;
+            }
+            catch (Exception e) {
+                /* 受け取らない */
+            }
+            return false;
+        }
+    }
+
+    public void catchLoadItem(Object item) {
+        @SuppressWarnings("unchecked")
+        List<File> files = (List<File>) item;
+        // 一番先頭のファイルを取得
+        if ((files != null) && (files.size() > 0)) {
+            for (File f : files) {
+                SystemProperties.getInstance().getPreloadFiles().add(f);
+                setSystemTableParam(SystemProperties.SYSP_FILE_DEFAULT_PATH, f.getParent());
+            }
+            setVisible(false);
+            commit();
+            targetPlg.startRendererWindow();
+        }
     }
 }
