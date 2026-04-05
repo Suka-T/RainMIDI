5584f34e53141eba620e4c5d09673843099f7647
Suka-T <absolute6626x@gmail.com>
2026-04-06 01:16:07 +0900

    ビルド内に最新マニュアルをコピーするようにした

--- RainMIDI_Gradle/make.bat
+++ RainMIDI_Gradle/make.bat
@@ -6,9 +6,10 @@ copy .\RainMIDI_launcher\build\libs\RainMIDI_launcher-all-1.0.jar .\RainMIDI_vX.
 
 xcopy .\RainMIDI_launcher\ext\res .\RainMIDI_vX.XX\res /S /E /I /H /Y
 xcopy .\RainMIDI_launcher\ext\jre .\RainMIDI_vX.XX\jre /S /E /I /H /Y
-copy .\RainMIDI_launcher\ext\*.html .\RainMIDI_vX.XX
 copy .\RainMIDI_launcher\ext\*.bat .\RainMIDI_vX.XX
 copy .\RainMIDI_launcher\ext\*.exe .\RainMIDI_vX.XX
 copy .\RainMIDI_launcher\ext\*.txt .\RainMIDI_vX.XX
+copy ..\docs\index.html .\RainMIDI_vX.XX\manual.html
+copy ..\docs\index.en.html .\RainMIDI_vX.XX\manual.en.html
 
 pause
