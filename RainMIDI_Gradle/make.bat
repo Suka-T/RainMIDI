27912c687edbd273cafcd3b9df53d37403e607a7
Suka-T <absolute6626x@gmail.com>
2026-04-05 23:21:57 +0900

    成果物生成batを作成

--- RainMIDI_Gradle/make.bat
+++ RainMIDI_Gradle/make.bat
@@ -1,3 +1,14 @@
-.\gradlew.bat :RainMIDI_launcher:fatJar
+call .\gradlew.bat :RainMIDI_launcher:fatJar
+
+mkdir RainMIDI_vX.XX
+
+copy .\RainMIDI_launcher\build\libs\RainMIDI_launcher-all-1.0.jar .\RainMIDI_vX.XX\RainMIDI.jar
+
+xcopy .\RainMIDI_launcher\ext\res .\RainMIDI_vX.XX\res /S /E /I /H /Y
+xcopy .\RainMIDI_launcher\ext\jre .\RainMIDI_vX.XX\jre /S /E /I /H /Y
+copy .\RainMIDI_launcher\ext\*.html .\RainMIDI_vX.XX
+copy .\RainMIDI_launcher\ext\*.bat .\RainMIDI_vX.XX
+copy .\RainMIDI_launcher\ext\*.exe .\RainMIDI_vX.XX
+copy .\RainMIDI_launcher\ext\*.txt .\RainMIDI_vX.XX
 
 pause
