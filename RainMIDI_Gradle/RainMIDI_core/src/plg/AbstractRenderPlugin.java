9855699b440da4feebca5e2dd5ba7cfbb225c6f6
Suka-T <absolute6626x@gmail.com>
2026-06-21 02:00:40 +0900

    v1.16

--- RainMIDI_Gradle/RainMIDI_core/src/plg/AbstractRenderPlugin.java
+++ RainMIDI_Gradle/RainMIDI_core/src/plg/AbstractRenderPlugin.java
@@ -30,7 +30,7 @@ import plg.SystemProperties.SyspViewMode;
 public class AbstractRenderPlugin extends JMidiPlugin implements IPlayerListener, ISupportExtensionConstraints {
     
     public static final String APP_NAME = "Rain MIDI";
-    public static final String APP_VERSION = "1.15";
+    public static final String APP_VERSION = "1.16";
     public static final String APP_YEAR = "2025";
     public static final String APP_COMPANY = "Suka";
 
