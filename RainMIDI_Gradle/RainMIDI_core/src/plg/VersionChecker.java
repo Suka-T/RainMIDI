8c49ac2942d9e67428c75dc44f840acc2b4b6f46
Suka-T <absolute6626x@gmail.com>
2026-06-21 01:40:17 +0900

    バージョンチェックのHTTP通信を初回のみにする

--- RainMIDI_Gradle/RainMIDI_core/src/plg/VersionChecker.java
+++ RainMIDI_Gradle/RainMIDI_core/src/plg/VersionChecker.java
@@ -5,9 +5,14 @@ import java.io.InputStreamReader;
 import java.net.URL;
 
 public class VersionChecker {
+	
+	private static boolean checkFlag = false;
+	private static boolean isExistsLatest = false;
     
-    public static boolean existsLatestVersion() {
-        String latest = getLatestVersion();
+	private static boolean existsLatestVersionImpl() {
+		String latest = getLatestVersion();
+		checkFlag = true;
+		
         if (latest == null) {
             return false;
         }
@@ -16,9 +21,17 @@ public class VersionChecker {
             return false;
         }
         return true;
+	}
+	
+    public static boolean existsLatestVersion() {
+    	if (!checkFlag) {
+    		// HTTP通信は起動してから１回だけにする
+    		isExistsLatest = existsLatestVersionImpl();
+    	}
+        return isExistsLatest;
     }
 
-    public static String getLatestVersion() {
+    private static String getLatestVersion() {
         try {
             @SuppressWarnings("deprecation")
             URL url = new URL("https://api.github.com/repos/Suka-T/RainMIDI/releases/latest");
@@ -38,7 +51,7 @@ public class VersionChecker {
         }
     }
     
-    public static String extractTagName(String json) {
+    private static String extractTagName(String json) {
         String key = "\"tag_name\":\"";
         int start = json.indexOf(key);
         if (start == -1) return null;
